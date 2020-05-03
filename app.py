c7d6c07f51db49a466ab19a02aec433f373e99f0
Dídac Sementé Fernández <didac.semente@gmail.com>
2020-05-03 19:50:48 +0200

    Created initial resources

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 #!/usr/bin/env python3
 
+import os
+
 from aws_cdk import core
+import aws_cdk.aws_ec2 as ec2
+import aws_cdk.aws_s3 as s3
+import aws_cdk.aws_kinesisfirehose as firehose
+import aws_cdk.aws_elasticsearch as es
 
-from logging_cdk.logging_cdk_stack import LoggingCdkStack
 
+class LoggignResources(core.Stack):
+    def __init__(self, *args, **kwargs):
+        super(LoggignResources, self).__init__(*args, **kwargs)
 
-app = core.App()
-LoggingCdkStack(app, "logging-cdk")
+        # Netowrk
+        self.vpc = ec2.Vpc(self, 'logging-vpc')
 
+        self.backup_bucket = s3.Bucket(self, 'logging-backup', 
+            bucket_name='logging-backup-bucket')
+
+        self.elastic_domain = es.CfnDomain(self, 'logging-es-cluster')
+
+        self.stream = firehose.CfnDeliveryStream(self, 'logging-stream',
+            delivery_stream_name='logging-stream-firehose',
+            delivery_stream_type='DirectPut', 
+            elasticsearch_destination_configuration=self.elastic_domain,
+            s3_destination_configuration=self.backup_bucket)
+
+class LoggingApp(core.App):
+    def __init__(self, *args, **kwargs):
+        super(LoggingApp, self).__init__(*args, **kwargs)
+
+        self.logging_resources = LoggignResources(self, 'logging-resources')
+
+app = LoggingApp()
 app.synth()
